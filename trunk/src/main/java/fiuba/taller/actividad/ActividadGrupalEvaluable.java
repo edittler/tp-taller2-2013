9d9d698cd49fa6fd8dacd70a2796688838b477af
ezeperez26 <ezeperez26@baaf28ec-c494-569a-7738-7e1c3d351155>
2013-11-30 14:54:00 +0000

    Agrego los métodos estáticos "esTipoValido" y "getActividad" que devuelve el tipo de clase correspondiente a la clase en que es llamado el método. Paso las constantes a sus respectivas clases derivadas.

--- trunk/src/main/java/fiuba/taller/actividad/ActividadGrupalEvaluable.java
+++ trunk/src/main/java/fiuba/taller/actividad/ActividadGrupalEvaluable.java
@@ -1,19 +1,43 @@
 package fiuba.taller.actividad;
 
-public class ActividadGrupalEvaluable extends ActividadGrupal implements IEvaluable {
+public class ActividadGrupalEvaluable extends ActividadGrupal implements
+		IEvaluable {
+
+	protected static final String TIPO_ACTIVIDAD_GRUPAL_EVALUABLE = TIPO_ACTIVIDAD_GRUPAL
+			+ " Evaluable";
 
 	public ActividadGrupalEvaluable() {
-		
+		super();
+		tipo = TIPO_ACTIVIDAD_GRUPAL_EVALUABLE;
 	}
-	
+
 	public void evaluar(long idEvaluado, String nota) {
+		// TODO Implementar
 	}
-	
+
 	public String getNota(long idParticipante) {
+		// TODO Implementar
 		return null;
 	}
-	
+
 	public String getNotas() {
+		// TODO Implementar
 		return null;
+	}
+
+	public static boolean esTipoValido(String xml) {
+		/*
+		 * TODO Implementar Se debe verificar que el tipo almacenado en el xml
+		 * contiene a TIPO_ACTIVIDAD_GRUPAL_EVALUABLE. No precisamente debe ser
+		 * igual, ya que pueden haber clases hijas.
+		 */
+		return true;
+	}
+
+	public static ActividadGrupalEvaluable getActividad(long idActividad) {
+		ActividadGrupalEvaluable actividad = new ActividadGrupalEvaluable();
+		String xml = actividad.getXml(idActividad);
+		actividad.descerializar(xml);
+		return actividad;
 	}
 }
