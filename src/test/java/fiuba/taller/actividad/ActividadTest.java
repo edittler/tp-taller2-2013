699cac9e1f8d0e20397544571e55860f0dc22f7d
ezeperez26 <ezeperez26@baaf28ec-c494-569a-7738-7e1c3d351155>
2013-12-03 22:52:05 +0000

    Corrijo la descerializacion de Actividad y agrego los tests pertinentes que verifican eso.

--- src/test/java/fiuba/taller/actividad/ActividadTest.java
+++ src/test/java/fiuba/taller/actividad/ActividadTest.java
@@ -1,6 +1,6 @@
 package fiuba.taller.actividad;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import fiuba.taller.actividad.excepcion.XmlErroneoExcepcion;
 
-public class PruebasActividad {
+public class ActividadTest {
 	Actividad act;
 	@Before
 	public void setUp() throws Exception {
@@ -43,6 +43,7 @@ public class PruebasActividad {
 		
 		act.descerializar(xmlADescerializar);
 		
+		assertEquals("id esperado: "+idPrueba+" id encontrado: "+act.getId(), act.getId(), idPrueba);
 		if (act.getId() != idPrueba){
 			fail("id esperado: "+idPrueba+" id encontrado: "+act.getId());
 		}
@@ -68,6 +69,65 @@ public class PruebasActividad {
 			fail("fecha fin distinta");
 		}
 		
+	}
+
+	@Test(expected=XmlErroneoExcepcion.class)
+	public void testDescerializarConXmlSinActividadCorrecta() throws XmlErroneoExcepcion {
+		long idPrueba = 22;
+		long idAmbSupStr = 99;
+		long idActSupStr = 77;
+		String nombrePrueba = "langosta";
+		String tipo = "bay guy";
+		String descripcion = "nada q ver nada q oler";
+		String fechaIni = "11/11/11";
+		String fechaFin = "12/12/12";
+		
+		String xmlADescerializar = "<?xml version=\"1.0\"?><WS><Actividadd>"
+				+ "<Id>" + idPrueba + "</Id>" 
+				+ "<IdAmbitoSuperior>" + idAmbSupStr + "</IdAmbitoSuperior>"
+				+ "<IdActividadSuperior>" + idActSupStr + "</IdActividadSuperior>"
+				+ "<Nombre>" + nombrePrueba + "</Nombre>"
+				+ "<Tipo>"+ tipo + "</Tipo>" 
+				+ "<Descripcion>"+ descripcion + "</Descripcion>" 
+				+ "<FechaInicio>"+ fechaIni + "</FechaInicio>" 
+				+ "<FechaFin>"+ fechaFin + "</FechaFin>" 
+				+ "</Actividadd></WS>";
+		act.descerializar(xmlADescerializar);
+	}
+
+	@Test(expected=XmlErroneoExcepcion.class)
+	public void testDescerializarConXmlConDosActividades() throws XmlErroneoExcepcion {
+		long idPrueba = 22;
+		long idAmbSupStr = 99;
+		long idActSupStr = 77;
+		String nombrePrueba = "langosta";
+		String tipo = "bay guy";
+		String descripcion = "nada q ver nada q oler";
+		String fechaIni = "11/11/11";
+		String fechaFin = "12/12/12";
+		
+		String xmlADescerializar = "<?xml version=\"1.0\"?><WS><Actividad>"
+				+ "<Id>" + idPrueba + "</Id>" 
+				+ "<IdAmbitoSuperior>" + idAmbSupStr + "</IdAmbitoSuperior>"
+				+ "<IdActividadSuperior>" + idActSupStr + "</IdActividadSuperior>"
+				+ "<Nombre>" + nombrePrueba + "</Nombre>"
+				+ "<Tipo>"+ tipo + "</Tipo>"
+				+ "<Descripcion>"+ descripcion + "</Descripcion>"
+				+ "<FechaInicio>"+ fechaIni + "</FechaInicio>"
+				+ "<FechaFin>"+ fechaFin + "</FechaFin>" 
+				+ "</Actividad>"
+				+ "<Actividad>"
+				+ "<Id>" + idPrueba + "</Id>"
+				+ "<IdAmbitoSuperior>" + idAmbSupStr + "</IdAmbitoSuperior>"
+				+ "<IdActividadSuperior>" + idActSupStr + "</IdActividadSuperior>"
+				+ "<Nombre>" + nombrePrueba + "</Nombre>"
+				+ "<Tipo>"+ tipo + "</Tipo>"
+				+ "<Descripcion>"+ descripcion + "</Descripcion>"
+				+ "<FechaInicio>"+ fechaIni + "</FechaInicio>"
+				+ "<FechaFin>"+ fechaFin + "</FechaFin>"
+				+ "</Actividad>"
+				+ "</WS>";
+		act.descerializar(xmlADescerializar);
 	}
 
 	@Test
