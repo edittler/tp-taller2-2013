fe16d52fd7f23f3bb12b1d1ac6e231b6c5a545ec
ezeperez26 <ezeperez26@baaf28ec-c494-569a-7738-7e1c3d351155>
2013-12-09 05:05:09 +0000

    + Completo algunas cosas de la clase Grupo:      + Agrego las excepciones a lanzar en los métodos "agregarParticipante" y "eliminarParticipante".      + Agrego la serializacion y descerializacion del atributo "idActividad"      + Simplificación del método "contieneParticipantesDe"      + Agrego el método "contieneParticipante" y "tamanio" + Agrego constructores que reciben un mensaje en 2 Excepciones + Corrijo los nombres de los tests de NotaIndividual

--- trunk/src/main/java/fiuba/taller/actividad/excepcion/ParticipanteInexistenteExcepcion.java
+++ trunk/src/main/java/fiuba/taller/actividad/excepcion/ParticipanteInexistenteExcepcion.java
@@ -6,4 +6,7 @@ public class ParticipanteInexistenteExcepcion extends RemoteException {
 
 	private static final long serialVersionUID = 3164996661911033429L;
 
+	public ParticipanteInexistenteExcepcion(String mensaje) {
+		super(mensaje);
+	}
 }
